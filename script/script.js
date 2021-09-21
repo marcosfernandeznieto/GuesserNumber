49210e25afa8c26b93c4b05a72e03cb6cdd12c23
Marcos <marcos.fernandez@softwareone.com>
2021-09-21 12:46:41 +0200

    proyecto terminado. Juego listo

--- script/script.js
+++ script/script.js
@@ -4,3 +4,33 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+function generateTarget() {
+    return Math.floor(Math.random()*(9-0)+0);
+}
+
+function compareGuesses(humanGuess, computerGuess, targetNumber) {
+    var diffHuman = getAbsoluteDistance(humanGuess,targetNumber);
+    var diffComputer = getAbsoluteDistance(computerGuess,targetNumber);
+
+    if (diffHuman>diffComputer) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function updateScore(winner) {
+    if (winner=="human") {
+        humanScore++;
+    } else {
+        computerScore++;
+    }
+}
+
+function advanceRound() {
+    currentRoundNumber++;
+}
+
+function getAbsoluteDistance(num1, num2) {
+    return Math.abs(num1-num2);
+}
